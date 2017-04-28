8a3c287f3418c1d1b430387941de542744cefc7f
hmj <445933532@qq.com>
2017-04-28 08:39:55 +0800

    增加查看api接口页面

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/api', function(req, res, next) {
+  res.render('api', { title: 'api' });
+});
+
 module.exports = router;
